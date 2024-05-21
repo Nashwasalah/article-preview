e1b721836c015a3616431c4a49fde1f961e35241
Nashwasalah <159080511+Nashwasalah@users.noreply.github.com>
2024-05-21 13:42:26 +0300

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,6 @@
+var shareBtn =document.getElementById('shareBtn')
+shareBtn.onclick =function(){
+    document.getElementById('shareIcons').style.visibility = 'visible'
+    //  document.getElementById('shareDiv').style.backgroundColor ='hsl(217, 19%, 35%)'
+    
+}
